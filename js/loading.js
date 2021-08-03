9ad54e701d32ab04930c5f694178e77ffbc40141
NeyLePro <84185453+Neylepro@users.noreply.github.com>
2021-08-03 11:20:28 +0200

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,14 @@
 function startLoading(){
 document.getElementsByClassName("ltext").innerHtml = "Loading Status..."
 
+  $.get(`https://api.bybilly.uk/api/players/${ip}/${port}`, (result) => {
+        if (result.hasOwnProperty('online')) {
+           var proxystatus = "Online"
+           var proxyonline = true
+        } else {
+           var proxystatus = "Offline"
+           var proxyonline = false
+
 }
  
 startLoading()
